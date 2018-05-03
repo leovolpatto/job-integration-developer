307026eee68e495e909444550a995a1b5dce6fbb
leovolpatto <leovolpatto@gmail.com>
2018-05-03 11:01:39 -0300

    Removing unused test methods and keep get() to make it easier to debug

---
+++ IntelipostMiddleware/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntelipostMiddleware.Controllers
+{
+    [Route("/")]
+    public class DefaultController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Service is running";
+        }
+    }
+}
